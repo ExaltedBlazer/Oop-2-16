5baf43f1cc446c1b53805aac2184c41c1a2dd1b7
Kaden Horrell-St.Cloud <death5819@gmail.com>
2026-02-24 21:24:40 -0500

    sixth commit OOP

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const Person = function (firstName, birthYear) {
   console.log(this);
   // Instance properties
@@ -121,3 +122,37 @@ console.log(tesla);
 tesla.accelerate();
 tesla.brake()
 
+*/
+
+// class expression
+//const PersonCl = class{}
+
+//class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.fName = firstName;
+    this.bYear = birthYear;
+  }
+
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.bYear);
+
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996)
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.fName}`);
+};
+
+jessica.greet();
+
+//1. Classes are NOT hoisted
+//2. Classes are first-class citizens
+//3. Classes are excecuted in strict mode
