01332e25880e2cb9e4e49e23a83b5dce2f995ccb
Kaden Horrell-St.Cloud <death5819@gmail.com>
2026-02-26 22:03:46 -0500

    eigth commit OOP

--- script.js
+++ script.js
@@ -129,8 +129,10 @@ tesla.brake()
 
 //class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.fName = firstName;
+  constructor(fullName, birthYear) {
+    // const fName;
+    // const bYear;
+    this.fName = fullName;
     this.bYear = birthYear;
   }
 
@@ -139,11 +141,32 @@ class PersonCl {
     console.log(2037 - this.bYear);
 
   }
+
+  greet() {
+    console.log(`Hey ${this.fName}`);
+
+  }
+
+  get age() {
+    return 2037 - this.bYear
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fName = name;
+    else alert(`${name} is not a full name!`)
+  }
+
+  get fullName() {
+    return this._fName
+  }
+
 }
 
-const jessica = new PersonCl('Jessica', 1996)
+const jessica = new PersonCl('Jessica Davis', 1996)
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -152,7 +175,26 @@ PersonCl.prototype.greet = function () {
 };
 
 jessica.greet();
-
+jessica.fullName = 'Jessica Davis';
 //1. Classes are NOT hoisted
 //2. Classes are first-class citizens
 //3. Classes are excecuted in strict mode
+
+const walter = new PersonCl('Walter', 1965);
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
